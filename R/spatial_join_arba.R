ed934a13388b0a4177bf1946c12477708b30ed22
M616 <52471892+M616@users.noreply.github.com>
2023-06-09 14:33:54 -0300

    Update spatial_join_arba.R

--- R/spatial_join_arba.R
+++ R/spatial_join_arba.R
@@ -1,8 +1,8 @@
 #' Spatial join with ARBA polygon base and attribute recoding
 #'
-#' This function performs a spatial join between a spatial object and the ARBA (Buenos Aires Revenue Agency) polygon base. It also recodes the resulting attributes using the official nomenclature.
+#' This function performs a spatial join between a spatial object and the ARBA polygon base. It also recodes the resulting attributes using the official nomenclature.
 #'
-#' @param x A spatial object representing points, lines, or polygons.
+#' @param data A spatial object representing points, lines, or polygons.
 #'
 #' @return A modified spatial object with additional attributes obtained from the spatial join with the ARBA polygon base.
 #'
@@ -12,16 +12,23 @@
 #' @import sf
 #' @importFrom utils download.file unzip
 
-spatial_join_arba <- function(x) {
-
+spatial_join_arba <- function(data) {
   # Convert the input object to sf and filter out rows with missing latitude or longitude values
-  x <- st_as_sf(x %>% filter(!is.na(latitude) & !is.na(longitude)),
-                coords = c('longitude', 'latitude'), remove = FALSE, crs = 4326)
+  data <- sf::st_as_sf(data %>% filter(!is.na(latitude) & !is.na(longitude)),
+                       coords = c('longitude', 'latitude'), remove = FALSE, crs = 4326)
 
-  # Download the ARBA polygon base and extract it to a temporary directory
+  # Create a temporary directory for storing the ARBA polygon base
   temp_dir <- tempdir()
-  temp_zip <- file.path(temp_dir, "limite_partidos.zip")
-  temp_folder <- unzip(temp_zip, exdir = temp_dir)
+
+  # URL of the ARBA polygon base ZIP file
+  arba_zip_url <- "https://catalogo.datos.gba.gob.ar/dataset/627f65de-2510-4bf4-976b-16035828b5ae/resource/2cc73f96-98f7-42fa-a180-e56c755cf59a/download/limite_partidos.zip"
+
+  # Download the ARBA polygon base ZIP file
+  arba_zip_file <- file.path(temp_dir, "arba_polygon_base.zip")
+  download.file(arba_zip_url, destfile = arba_zip_file)
+
+  # Extract the ZIP file to a temporary directory
+  temp_folder <- unzip(arba_zip_file, exdir = temp_dir)
 
   # Read the ARBA polygon base
   partidos_pba <- sf::st_read(temp_folder[4])
@@ -31,14 +38,17 @@ spatial_join_arba <- function(x) {
   partidos_pba <- sf::st_make_valid(partidos_pba)  # Ensure polygon validity
 
   # Perform spatial join with ARBA polygons
-  x <- sf::st_join(partidos_pba[c('nam', 'cca')], x, join = st_intersects)
+  data <- sf::st_join(partidos_pba[c('nam', 'cca')], data, join = sf::st_intersects)
 
   # Remove geometry column and rename attributes
-  x$geometry <- NULL
-  x$nombre_arba <- x$nam
-  x$nam <- NULL
-  x$arba_code <- x$cca
-  x$cca <- NULL
+  data$geometry <- NULL
+  data$nombre_arba <- data$nam
+  data$nam <- NULL
+  data$arba_code <- data$cca
+  data$cca <- NULL
 
-  return(x)
+  # Clean up the temporary directory
+  unlink(temp_dir, recursive = TRUE)
+
+  return(data)
 }
