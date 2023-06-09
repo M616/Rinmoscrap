18e209df12430024f6170016001044d1c41b3782
M616 <52471892+M616@users.noreply.github.com>
2023-06-09 11:58:53 -0300

    update

--- R/spatial_join_arba.R
+++ R/spatial_join_arba.R
@@ -1,47 +1,43 @@
-#' spatial_join_arba
+#' Spatial join with ARBA polygon base and attribute recoding
 #'
-#' @param x
-#' @description Spatial union with the ARBA polygon base and recoding with official nomenclature.
-#' @return
+#' This function performs a spatial join between a spatial object and the ARBA (Buenos Aires Revenue Agency) polygon base. It also recodes the resulting attributes using the official nomenclature.
+#'
+#' @param x A spatial object representing points, lines, or polygons.
+#'
+#' @return A modified spatial object with additional attributes obtained from the spatial join with the ARBA polygon base.
+#'
 #' @export
 #'
-#' @examples
-spatial_join_arba <- function(x)  {
+#' @import dplyr
+#' @import sf
+#' @importFrom utils download.file unzip
 
-  x <- st_as_sf (x %>% filter(!is.na(latitude)&!is.na(longitude) ), coords=c('longitude','latitude'),remove=FALSE, crs=4326)
-  #return(x)
-  #se descarga los poligonos de los partidos de arba
+spatial_join_arba <- function(x) {
+
+  # Convert the input object to sf and filter out rows with missing latitude or longitude values
+  x <- st_as_sf(x %>% filter(!is.na(latitude) & !is.na(longitude)),
+                coords = c('longitude', 'latitude'), remove = FALSE, crs = 4326)
+
+  # Download and read the ARBA polygon base
   temp <- tempfile()
-  utils::download.file ('https://catalogo.datos.gba.gob.ar/dataset/627f65de-2510-4bf4-976b-16035828b5ae/resource/2cc73f96-98f7-42fa-a180-e56c755cf59a/download/limite_partidos.zip',temp)
+  url <- 'https://catalogo.datos.gba.gob.ar/dataset/627f65de-2510-4bf4-976b-16035828b5ae/resource/2cc73f96-98f7-42fa-a180-e56c755cf59a/download/limite_partidos.zip'
+  utils::download.file(url, temp)
   out <- utils::unzip(temp)
-  partidos_pba <- st_read(out[4])
+  partidos_pba <- sf::st_read(out[4])
 
+  # Prepare ARBA polygons for the spatial join
   sf_use_s2(FALSE)
-  partidos_pba <- sf::st_make_valid(partidos_pba)
+  partidos_pba <- sf::st_make_valid(partidos_pba)  # Ensure polygon validity
 
-  #se renombran los poligonos de islas
-  #partidos_pba <- partidos_pba %>% mutate(nam=case_when(nam=='Islas Baradero'~'Baradero',
-  #                                              nam=='Islas Campana'~'Campana',
-  #                                              nam=='Islas de San Nicolas'~'San Nicolás',
-  #                                              nam=='Islas de Zárate'~'Zárate',
-  #                                              nam=='Islas Ramallo'~'Ramallo',
-  #                                              nam=='Islas San Fernando'~'San Fernando',
-  #                                              nam=='Islas San Pedro'~'San Pedro',
-  #                                              nam=='Islas Tigre'~'Tigre',
-  #                                              TRUE~nam) )
+  # Perform spatial join with ARBA polygons
+  x <- sf::st_join(partidos_pba[c('nam', 'cca')], x, join = st_intersects)
 
-  partidos_pba <- partidos_pba %>% filter(!is.na(geometry))
-
-  x <- sf::st_join(partidos_pba[c('nam','cca')],x,join=st_intersects)
-
+  # Remove geometry column and rename attributes
   x$geometry <- NULL
   x$nombre_arba <- x$nam
   x$nam <- NULL
   x$arba_code <- x$cca
   x$cca <- NULL
 
-  rm(partidos_pba,out,temp)
-
   return(x)
-
-  }
+}
