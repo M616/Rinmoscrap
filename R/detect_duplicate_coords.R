d1d6cf550c94bbc38d3c47e44b9244170301efd3
M616 <52471892+M616@users.noreply.github.com>
2023-07-11 15:15:33 -0300

    Update detect_duplicate_coords.R

--- R/detect_duplicate_coords.R
+++ R/detect_duplicate_coords.R
@@ -3,13 +3,19 @@
 #' This function takes a dataset containing geographic coordinates and checks
 #' for duplicate coordinate pairs. It then adds a new boolean column called
 #' 'is_duplicated_coord' that indicates whether a row has a duplicate
-#' coordinate (TRUE if it is a duplicate, FALSE otherwise).
+#' coordinate (TRUE if it is a duplicate, FALSE otherwise). It also adds an
+#' optional 'count_duplicates' column that represents the number of occurrences
+#' of each coordinate pair.
 #'
 #' @param data The dataset containing the geographic coordinates.
 #'        It should have at least two columns named 'latitude' and 'longitude'
 #'        representing the coordinates.
-#' @return The original dataset with an added 'is_duplicated_coord' column
-#'         of type logical.
+#' @param generate_count_logical A logical value indicating whether to generate
+#'        the 'count_duplicates' column. If TRUE, the 'count_duplicates' column
+#'        will be added to the output dataset (default is FALSE).
+#' @return The original dataset with an added 'is_duplicated_coord' column of
+#'         type logical. If 'generate_count_logical' is TRUE, the dataset will
+#'         also contain a 'count_duplicates' column of type integer.
 #'
 #' @examples
 #' data <- data.frame(latitude = c(37.7749, 40.7128, 37.7749, 34.0522),
@@ -22,12 +28,17 @@
 #' @import dplyr
 #' @importFrom sf st_as_sf st_sfc st_set_crs st_write
 #' @export
-detect_duplicate_coords <- function(data) {
+detect_duplicate_coords <- function(data, generate_count_logical = FALSE) {
   duplicated_coords <- data %>%
     group_by(latitude, longitude) %>%
-    mutate(is_duplicated_coord = n() > 1) %>%
-    ungroup()
+    mutate(is_duplicated_coord = n() > 1)
 
+  if (generate_count_logical) {
+    duplicated_coords <- duplicated_coords %>%
+      group_by(latitude, longitude) %>%
+      mutate(count_duplicates = n()) %>%
+      ungroup()
+  }
 
   return(duplicated_coords)
 }
