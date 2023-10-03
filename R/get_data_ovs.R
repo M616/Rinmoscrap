16274a46abcb8dc460086a99981d94ed8ff3b5da
M616 <52471892+M616@users.noreply.github.com>
2023-10-03 09:24:52 -0300

    Update get_data_ovs.R

--- R/get_data_ovs.R
+++ R/get_data_ovs.R
@@ -5,8 +5,6 @@
 #'
 #' @return An 'sf' object containing soil data.
 #'
-#' @details The function relies on the 'googledrive' and 'sf' packages. If these packages are not installed, they need to be
-#'   installed before using this function.
 #'
 #' @export
 #'
@@ -16,12 +14,8 @@
 #' head(data)
 #' }
 #'
-#' @seealso [googledrive](https://cran.r-project.org/package=googledrive)
-#' @seealso [sf](https://cran.r-project.org/package=sf)
 #'
-#' @import googledrive
 #' @import sf
-#'
 #'
 #' @param None
 #'
@@ -37,14 +31,15 @@
 #'
 
 get_data_ovs <- function() {
-  # Create a temporary file with a .zip extension
+  # Specify the direct download link of the file
+  direct_download_link <- "https://drive.google.com/uc?id=1Jql3hyztfuGKhXLzkWjYoCcXZ-fSaJ-S"
+
   temp <- tempfile(fileext = ".zip")
 
-  # Download the file from Google Drive using its ID
-  # Replace "1Jql3hyztfuGKhXLzkWjYoCcXZ-fSaJ-S" with the correct ID of the file on Google Drive
-  drive_download(as_id("1Jql3hyztfuGKhXLzkWjYoCcXZ-fSaJ-S"), path = temp, overwrite = TRUE)
+  # Download the file directly
+  download.file(url = direct_download_link, destfile = temp, mode = "wb")
 
-  # Unzip the .zip file into a temporary directory
+  # Read the downloaded .gpkg file into an 'sf' object
   data <- sf::st_read(unzip(temp, exdir = tempdir()))
 
   # Return the sf object
